3e3ccfc7c9faa55e7068d381582315ace8d7aea9
Isabela  Fagundes <104397121+isabelafagundes@users.noreply.github.com>
2023-06-13 18:32:18 -0300

    Update and rename AtualizaEmpresa.java to AtualizaEmpresaServlet.java

--- gerenciador_de_empresas_com_java_servlet/src/main/java/br/com/alura/gerenciador/servlet/AtualizaEmpresaServlet.java
+++ gerenciador_de_empresas_com_java_servlet/src/main/java/br/com/alura/gerenciador/servlet/AtualizaEmpresaServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/atualizaEmpresa")
-public class AtualizaEmpresa extends HttpServlet {
+public class AtualizaEmpresaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
